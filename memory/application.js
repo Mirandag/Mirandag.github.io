2b01ed1df4dc6c48885358640394e56037076538
Mirandag <mgenao@kippnyccp.org>
2015-03-13 09:03:05 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
   var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"];
 
-  for(i in card_array){
+  var click_count = 0;
+  for(i in card_array){ //creates the cards
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
-  $('.card').click(function(){
-    $(this).find('p').css('opacity', 1);
-    
+  $('.card').click(function(){ //when card is clicked...
+    if (click_count == 0){
+      $(this).find('p').css('opacity', 1); //shows what is behind the card
+      click_count = 1;
+    }
+    else{
+      
+    }
   });
 });
