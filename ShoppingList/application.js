dbc5fd1b3b0de34fa45aac3016ca878a1b9f0981
Mirandag <mgenao@kippnyccp.org>
2015-04-13 09:05:34 -0400

    Update application.js

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  $('#button').on('click',function(){
-    var item = $('#placeholder').val();
-    $('ul').append('<li>'+item+'</li');
-  });
-  });
+ $('#button').on('click',function(){
+  var item = $('#placeholder').val();
+  $('ul').append('<li>'+item+'</li>');
+ }); 
+ $('#clear').on('click',function(){
+  $('ul').empty();
+ }); 
+});
